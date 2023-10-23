75945d67a74fc51df07d3a06fb224756140062b7
aziqikang <aziqikang@gmail.com>
2023-10-22 21:53:34 -0400

    Update popup to show current tab url

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,26 @@
-chrome.tabs.query({
+// document.getElementById("addBookmark").addEventListener("click", addTab());
+
+const tabs = await chrome.tabs.query({
     active: true,
-    currentWindow: true
-}, function(tabs) {
-    var tabURL = tabs[0].url;
-    console.log(tabURL);
+    lastFocusedWindow: true
 });
 
-function addBookmark() {}
+const template = document.getElementById("li_template");
+const elements = new Set();
+for (const tab of tabs) {
+  const element = template.content.firstElementChild.cloneNode(true);
+
+  const title = tab.title.split("-")[0].trim();
+  const pathname = new URL(tab.url).pathname.slice("/docs".length);
+
+  element.querySelector(".tabname").textContent = title;
+  element.querySelector(".pathname").textContent = pathname;
+  element.querySelector("a").addEventListener("click", async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  elements.add(element);
+}
+document.querySelector("ul").append(...elements);
